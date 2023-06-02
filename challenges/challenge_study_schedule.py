6493c59ea662b807be9a1521632a0cc6b1bd35a7
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-06-02 20:00:27 -0300

    FEAT: REQ 1 DONE

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+    
+    repeat_times = 0
+
+    for student_permanence in permanence_period:
+        if not type(student_permanence[0]) is int or not type(student_permanence[1]) is int:
+            return None
+        
+        if student_permanence[0] <= target_time <= student_permanence[1]:
+            repeat_times += 1
+
+    return repeat_times
