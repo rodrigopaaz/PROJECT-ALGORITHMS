a4171acccb28241f9ac8dd88cc2acf9bb5bc8c4f
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-06-06 10:37:19 -0300

    feat: req 5 done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if nums == '':
+        return False
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    is_duplicate = False
+    for numb in numbers:
+        contador = nums.count(numb)
+        if contador > 1:
+            is_duplicate = numb
+    print(is_duplicate)
+    return is_duplicate
